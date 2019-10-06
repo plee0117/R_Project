ddb68a0eeac619466c7938cd73ce2a0d35a74e29
Paul <emailme.paul@gmail.com>
2019-10-06 17:46:03 -0400

    add shiny files

---
+++ ui.R
@@ -0,0 +1,20 @@
+
+
+shinyUI(fluidPage(
+
+    titlePanel("Crashes"),
+
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        mainPanel(
+            plotOutput("distPlot")
+        )
+    )
+))
